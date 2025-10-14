93b75cebf4a0ae5d216cb9c3daf5415ed70e7979
Soubhagya <dragonworld70659@gmail.com>
2025-10-14 12:45:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,11 +10,27 @@ API_ID = int(getenv("API_ID", "27079591")) #optional
 API_HASH = getenv("API_HASH", "c81ae4c3dc026ea4bf49842a8ce4a5f9") #optional
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "7403621976").split()))
-OWNER_ID = int(getenv("OWNER_ID"))
+OWNER_ID = int(getenv("OWNER_ID",'https://t.me/sexyspectorr')
 MONGO_URL = getenv("MONGO_URL")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
-ALIVE_PIC = getenv("ALIVE_PIC", 'https://telegra.ph/file/5badd2112e1e0cdf03a1f.jpg')
-ALIVE_TEXT = getenv("ALIVE_TEXT")
+ALIVE_PIC = getenv("ALIVE_PIC", 'https://files.catbox.moe/4b14yr.jpg')
+ALIVE_TEXT = getenv("ALIVE_TEXT,|────────────────────|
+| '⚡ ʜᴇʏ, ɪ ᴀᴍ : ˹ 𝐔sᴇʀʙᴏᴛ 𝐇ᴏsᴛᴇʀ ˼
+| ⚙️ ᴀᴅᴠᴀɴᴄᴇᴅ ᴀɴɪᴍᴀᴛɪᴏɴ + ғᴜɴ ᴛᴏᴏʟs
+| 🔐 ғᴀsᴛ • sᴇᴄᴜʀᴇ • ᴍᴏᴅᴜʟᴀʀ
+|────────────────────|
+
+| ʜᴏᴡ ᴛᴏ ᴜsᴇ ᴍᴇ: Click Tips
+| ɢᴇɴᴇʀᴀᴛᴇ ꜱᴇꜱꜱɪᴏɴ: @AnanyaSessionBot
+|────────────────────|
+| ᴄʟᴏɴᴇ ʙᴏᴛ ɪɴ 2 sᴇᴄᴏɴᴅs:
+| /clone session_string
+| ʀᴇᴍᴏᴠᴇ ᴄʟᴏɴᴇ:
+| /delclone session_string
+| /logout session_string
+|────────────────────|
+| ᴘᴏᴡᴇʀᴇᴅ ʙʏ: ˹Sona Bots˼
+|────────────────────|')
 PM_LOGGER = getenv("PM_LOGGER")
 LOG_GROUP = getenv("LOG_GROUP")
 GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
